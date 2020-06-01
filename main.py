1643634634d306477cb8c7d8a78e9ba318f3cf41
CoffeeStraw <clarantonio98@gmail.com>
2020-06-01 20:04:21 +0200

    Implemented lr_decay, cleaned up arguments parser, added callback to save training weights

--- main.py
+++ main.py
@@ -35,11 +35,19 @@ def load_mnist():
 
 def train(model, data, args):
     """Perform training, saving weights at the end of all the epochs
-    """
 
+    Args:
+        model: The CapsuleNet model to train.
+        data: The dataset that you want to train: ((x_train, y_train), (x_test, y_test))
+        args: The arguments parsed at the beginning of the script.
+
+    Returns:
+        The trained model
+    """
     # Unpack data
     (x_train, y_train), (x_test, y_test) = data
 
+    # Define margin_loss function
     def margin_loss(y_true, y_pred):
         """Local function for margin loss, Eq.(4).
 
@@ -63,11 +71,33 @@ def train(model, data, args):
                   loss_weights=1.,
                   metrics=['accuracy'])
 
-    # Training without data augmentation:
-    model.fit(x=x_train, y=y_train, batch_size=args.batch_size, epochs=args.epochs,
-              validation_data=(x_test, y_test))
+    # Define a callback to reduce learning rate
+    lr_decay = callbacks.LearningRateScheduler(
+        schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
 
+    # Define a callback that will save weights after every `args.save_freq` batches done
+    class WeightsSaver(callbacks.Callback):
+        def __init__(self, save_dir, save_freq):
+            self.save_dir = save_dir
+            self.save_freq = save_freq
+            self.epoch = 1
+
+        def on_batch_end(self, batch, logs={}):
+            if batch % self.save_freq == 0:
+                filename = f'{self.save_dir}/{self.epoch}-{batch}.h5'
+                self.model.save_weights(filename)
+
+        def on_epoch_end(self, epoch, logs={}):
+            self.epoch += 1
+
+    # Simple training without data augmentation:
+    model.fit(x=x_train, y=y_train, batch_size=args.batch_size, epochs=args.epochs,
+              validation_data=(x_test, y_test),
+              callbacks=[lr_decay, WeightsSaver(args.training_save_dir, args.save_freq)])
+
+    # Save final weights at the end of the training
     model.save_weights(args.save_dir + '/trained_model.h5')
+    return model
 
 
 def test(model, data, args):
@@ -82,10 +112,16 @@ if __name__ == "__main__":
     (x_train, y_train), (x_test, y_test) = load_mnist()
 
     # Instantiate Capsule Network Model
-    model = CapsuleNet(input_shape=x_train.shape[1:], n_class=y_train.shape[1], r_iter=3)
+    model = CapsuleNet(
+        input_shape=x_train.shape[1:],
+        n_class=y_train.shape[1],
+        r_iter=3)
 
     # Show a complete summary
     model.summary(batch_size=args.batch_size)
 
     # Train!
-    train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
+    model = train(
+        model=model,
+        data=((x_train, y_train), (x_test, y_test)),
+        args=args)
--- utils.py
+++ utils.py
@@ -10,32 +10,43 @@ import argparse
 def parse_args():
     # Getting/setting the hyper-parameters
     parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
-    parser.add_argument('--epochs', default=50, type=int)
-    parser.add_argument('--batch_size', default=100, type=int)
+    
+    # General
+    parser.add_argument('--save_dir', default='result', help="The directory that will contains every output of the execution.")
+    parser.add_argument('--save_freq', default=10, type=int, help="The number of batches after which weights are saved.")
+
+    # Testing or training?
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+
+    # Initial weights?
+    parser.add_argument('-w', '--weights', default=None,
+                    help="The path of the saved weights. Should be specified when testing")
+
+    # Training
+    parser.add_argument('--epochs', default=50, type=int, help="Number of epochs for the training.")
+    parser.add_argument('--batch_size', default=100, type=int, help="Size of the batch used for the training.")
     parser.add_argument('--lr', default=0.001, type=float,
                         help="Initial learning rate")
     parser.add_argument('--lr_decay', default=0.9, type=float,
                         help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    
+    # Capsule Network
     parser.add_argument('--lam_recon', default=0.392, type=float,
                         help="The coefficient for the loss of decoder")
     parser.add_argument('-r', '--routings', default=3, type=int,
                         help="Number of iterations used in routing algorithm. should > 0")
-    parser.add_argument('--shift_fraction', default=0.1, type=float,
-                        help="Fraction of pixels to shift at most in each direction.")
-    parser.add_argument('--debug', action='store_true',
-                        help="Save weights by TensorBoard")
-    parser.add_argument('--save_dir', default='./result')
-    parser.add_argument('-t', '--testing', action='store_true',
-                        help="Test the trained model on testing dataset")
-    parser.add_argument('--digit', default=5, type=int,
-                        help="Digit to manipulate")
-    parser.add_argument('-w', '--weights', default=None,
-                        help="The path of the saved weights. Should be specified when testing")
 
+    # Parse arguments from command line
     args = parser.parse_args()
 
-    # Creating results directory, if it doesn't exist
+    # Construct save dir path for training weights
+    args.training_save_dir = os.path.join(args.save_dir, 'training_weights')
+
+    # Creating results directories, if they do not exist
     if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
+        os.mkdir(args.save_dir)
+    if not os.path.exists(args.training_save_dir):
+        os.mkdir(args.training_save_dir)
 
     return args
