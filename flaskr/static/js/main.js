7cb31dd0e924f6aec2bea207f266d9f7247006bb
CoffeeStraw <clarantonio98@gmail.com>
2020-07-29 23:46:46 +0200

    Separated visualizer logic in javascript files

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -3,6 +3,7 @@
  * @author Antonio Strippoli
  */
 import { TrainingStepSelector, ModelSelector } from './core/selectors.js'
+import { visualizeComputeStep, visualizeLayersTabs } from './core/visualizer.js'
 
 async function main() {
     // Request models' structure from API
@@ -39,36 +40,8 @@ async function main() {
             // Update training step selector
             tss.changeElements(models[ms.selected]['training_steps'])
 
-            // Prepare visualization section for the selected model
-            var visualization_html = ''
-            var visualization_content_html = ''
-
-            models[ms.selected]['layers'].forEach(function (value, i) {
-                var [name, type] = value
-                if (i == 0) {
-                    visualization_content_html += `
-                    <div class="tab-pane fade text-center show active" id="${name}" role="tabpanel" aria-labelledby="${name}-tab">Send an image!</div>`
-
-                    visualization_html += `
-                    <li class="nav-item">
-                            <a class="nav-link active show" id="${name}-tab" data-toggle="tab" href="#${name}"
-                            role="tab" aria-controls="${name}" aria-selected="true">${name}</a>
-                    </li>`
-                } else {
-                    visualization_content_html += `
-                    <div class="tab-pane fade text-center" id="${name}" role="tabpanel" aria-labelledby="${name}-tab">Send an image!</div>`
-
-                    visualization_html += `
-                    <li class="nav-item">
-                        <a class="nav-link"  id="${name}-tab" data-toggle="tab" href="#${name}"
-                        role="tab" aria-controls="${name}" aria-selected="false">${name}</a>
-                    </li>`
-                }
-            })
-
-            // Update htmls
-            $('#visualization').html(visualization_html)
-            $('#visualization-content').html(visualization_content_html)
+            // Visualize tabs for the current model
+            visualizeLayersTabs(models[ms.selected]['layers'])
         }
     );
 
@@ -97,41 +70,8 @@ async function main() {
             })
         )
 
-        // Set input-img with the choosen one and attach the current time as query string to force no caching
-        document.getElementById("input-img").src = response['out_dir'] + "img.jpeg?" + performance.now()
-
-        // Visualize every image produced
-        for (var layer_name in response['layers_outs']) {
-            var out_params = response['layers_outs'][layer_name]
-            if (out_params == null)
-                continue
-
-            // Get image path and attach the current time as query string to force no caching
-            var img_path = response['out_dir'] + `${layer_name}/` + out_params['outs'] + "?" + performance.now()
-
-            // Custom zoom and margin for better visualization
-            var zoom = 100 / out_params['chunk_width']
-            var margin = 5 / zoom
-
-            var visualization_content_html = ''
-            for (var row = 0; row < out_params['rows']; row++) {
-                for (var col = 0; col < out_params['cols']; col++) {
-                    visualization_content_html += `
-                    <img class="img-responsive img-gallery" src="/static/img/tmp.gif"
-                    style="background: url(${img_path});
-                           background-position: -${col * out_params['chunk_width']}px -${row * out_params['chunk_height']}px;
-                           width: ${out_params['chunk_width']}px;
-                           height: ${out_params['chunk_height']}px;
-                           zoom: ${zoom};
-                           margin: ${margin}px ${margin}px;
-                    ">`
-                }
-                visualization_content_html += "<br/>"
-            }
-            console.log(visualization_content_html)
-
-            $(`#${layer_name}`).html(visualization_content_html)
-        }
+        // Visualize input image and outputs in the tabs
+        visualizeComputeStep(response)
     })
 }
 
